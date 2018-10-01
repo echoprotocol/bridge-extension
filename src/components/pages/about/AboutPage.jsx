2c8898b4de67888c19d81b4707568321e4c31f44
Andrey Fedorovich <afedorovich@Andreys-iMac.local>
2018-10-01 13:28:32 +0300

    BRIDGE-5

--- src/components/pages/about/AboutPage.jsx
+++ src/components/pages/about/AboutPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Input, Button } from 'semantic-ui-react';
+import classnames from 'classnames';
 
 class AboutPage extends React.Component {
 
@@ -10,8 +11,11 @@ class AboutPage extends React.Component {
 			<div className="about-page">
 				<div className="wrap">
 					<div className="input-wrap">
-						<Input />
-
+						<Input
+							label="Repeat PIN"
+							onChange={(e, data) => this.onChange(e, data)}
+							className={classnames({ dirty: true })}
+						/>
 					</div>
 					<div className="one-btn-wrap">
 						<Button className="btn-in-light" content="test" />
