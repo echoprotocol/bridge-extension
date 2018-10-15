f37e5e1b7ee57fc9085678e1a2685fc57af886c3
Nikita Pevnev <thystruby@gmail.com>
2018-10-15 10:05:38 +0300

    upd

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -24,7 +24,7 @@ class App extends React.Component {
 				<div className={classnames('app-wrap', 'dark')} >
 					{/* <Header />
 					<Navbar /> */}
-					<div className="header_bridge-image">
+					<div className="header-bridge-image">
 						<span>Bridge</span>
 					</div>
 					{children}
