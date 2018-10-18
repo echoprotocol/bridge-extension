33308c66797707ebc0311f1c44f2df7966cca2df
Andrey Fedorovich <afedorovich@Andreys-iMac.local>
2018-10-18 18:14:51 +0300

    BRG-61

--- src/components/SuccessTransaction/index.jsx
+++ src/components/SuccessTransaction/index.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Button } from 'semantic-ui-react';
 
 class SuccessTransaction extends React.PureComponent {
 
 	render() {
 		return (
 			<div className="success-wrap">
-				<div className="plane" />
+				<div className="page-action-wrap">
+					<div className="one-btn-wrap">
+						<Button
+							className="btn-inverted success"
+							content={<span className="btn-text">Proceed</span>}
+						/>
+					</div>
+				</div>
 			</div>
 		);
 	}
