05bd9ef3cb1f466416eb6dbc47e382113fe4400d
Andrey Fedorovich <afedorovich@Andreys-iMac.local>
2018-10-22 15:09:53 +0300

    BRG-67

--- src/components/UserIcon/index.jsx
+++ src/components/UserIcon/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import classnames from 'classnames';
+import { Button } from 'semantic-ui-react';
 
 class UserIcon extends React.PureComponent {
 
@@ -23,24 +24,28 @@ class UserIcon extends React.PureComponent {
 	render() {
 
 		return (
-			<div
+			<Button
+				tabIndex={this.props.tabSelect ? null : '-1'}
 				className={classnames(
 					'user-icon-wrap',
 					{ big: this.props.big },
-
-					{ 'change-animation': this.props.animationChange },
-
 					{ medium: this.props.medium },
-
+					{ 'change-animation': this.props.animationChange },
+					{ select: this.props.select },
+					{ active: this.props.active },
 					this.props.color,
 				)}
 			>
 				<div className="content">
-					<i aria-hidden="true" className={classnames(`icon-${this.props.avatar}`)} />
+					<i
+
+						aria-hidden="true"
+						className={classnames(`icon-${this.props.avatar}`)}
+					/>
 				</div>
 				{ this.props.animationChange ? this.renderAnimationChange() : null }
 
-			</div>
+			</Button>
 
 		);
 	}
@@ -51,15 +56,21 @@ UserIcon.propTypes = {
 	avatar: PropTypes.string.isRequired,
 	color: PropTypes.string,
 	big: PropTypes.bool,
-	animationChange: PropTypes.bool,
 	medium: PropTypes.bool,
+	animationChange: PropTypes.bool,
+	select: PropTypes.bool,
+	active: PropTypes.bool,
+	tabSelect: PropTypes.bool,
 };
 UserIcon.defaultProps = {
 	color: 'green',
 	big: false,
-	animationChange: false,
-
 	medium: false,
+	animationChange: false,
+	select: false,
+	active: false,
+	tabSelect: false,
+
 };
 export default connect(
 	() => ({
--- src/containers/SettingsAccount/index.jsx
+++ src/containers/SettingsAccount/index.jsx
@@ -1,17 +1,66 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react';
+
 import UserIcon from '../../components/UserIcon';
 
 class SettingsAccount extends React.Component {
 
 	render() {
+		// ЦВЕТА ИМЕННО В ТАКОМ ПОРЯДКЕ !!!
+		const colors = ['green', 'sky', 'blue', 'pink', 'red', 'yellow', 'lemon'];
 		return (
 			<React.Fragment>
 
 				<div className="settings-wrap">
-					<UserIcon color="green" big animationChange avatar="ava1" />
+					<UserIcon color="green" big remove avatar="ava1" />
 					<div className="page-wrap" >
-						<div className="page" >3</div>
-						<div className="page-action-wrap">3</div>
+						<div className="page">
+							<ul className="list-avatars">
+
+								{
+									Array(15).fill().map((elm, i) => {
+										const id = i;
+										return (
+											<li key={id} >
+												<UserIcon
+													select
+													tabSelect
+													active={i === 0}
+													color="transparent"
+													avatar={`ava${i + 1}`}
+												/>
+											</li>
+										);
+									})
+								}
+							</ul>
+							<ul className="list-colors">
+								{
+									colors.map((elm, i) => {
+										const id = i;
+										return (
+											<li key={id} >
+												<Button
+													active={i === 0}
+													className={`select-${elm}`}
+												/>
+											</li>
+										);
+									})
+								}
+							</ul>
+						</div>
+						<div className="page-action-wrap">
+							<div className="one-btn-wrap">
+								<Button
+									className="btn-in-light"
+									disabled
+									content={
+										<span className="btn-text">Save Changes</span>
+									}
+								/>
+							</div>
+						</div>
 					</div>
 				</div>
 			</React.Fragment>
