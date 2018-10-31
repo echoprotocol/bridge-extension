ab3cc9d26c79dd04a13f4dc0ed078c84d8988c0b
Dmitry Karpushkin <d.karpushkin@pixelplex.by>
2018-10-31 15:49:54 +0300

    fix

---
+++ src/helpers/ValidateSend.js
@@ -0,0 +1,30 @@
+class ValidateSend {
+
+	static amountInput(value, currency) {
+		if (!value.match(/^[0-9]*[.,]?[0-9]*$/)) {
+			dispatch(setFormError(form, 'amount', 'Amount must contain only digits and dot'));
+			return;
+		}
+
+		if (value.replace(',', '.') !== '' && !Math.floor(value.replace(',', '.') * (10 ** currency.precision))) {
+			dispatch(setFormError(
+				form,
+				'amount',
+				`Amount should be more than 0 (${currency.symbol} precision is ${currency.precision} symbols)`,
+			));
+		}
+
+		if (/\.|,/.test(value)) {
+			const [intPath, doublePath] = value.split(/\.|,/);
+			value = `${intPath ? Number(intPath) : ''}.${doublePath || ''}`;
+		} else {
+			value = value ? Number(value).toString() : value;
+		}
+
+		dispatch(setFormError(form, 'fee', null));
+		dispatch(setFormValue(form, name, value));
+	}
+
+}
+
+export default ValidateSend;
