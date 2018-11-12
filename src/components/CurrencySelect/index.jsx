21db0f7065bb7c81a7e570e1e5e226fc47a2a337
Nikita P <thystruby@gmail.com>
2018-11-12 16:27:06 +0300

    BRG-75 update

--- src/components/CurrencySelect/index.jsx
+++ src/components/CurrencySelect/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Input } from 'semantic-ui-react';
-import { Dropdown, MenuItem } from 'react-bootstrap';
+import { Dropdown } from 'react-bootstrap';
 import CustomScroll from 'react-custom-scroll';
+import { KEY_CODE_TAB, KEY_CODE_SPACE, KEY_CODE_ENTER } from '../../constants/GlobalConstants';
 
 import CustomMenu from './CustomMenu';
 
@@ -29,14 +29,25 @@ class CurrencySelect extends React.Component {
 		document.removeEventListener('mousedown', this.handleClickOutside);
 	}
 
+	onItemKeyPress(e, text) {
+		const code = e.keyCode || e.which;
+
+		if (this.state.opened && [KEY_CODE_ENTER, KEY_CODE_SPACE].includes(code)) {
+			e.preventDefault();
+			this.handleClick(text);
+		}
+	}
+
 	setMenuRef(node) {
 		this.menuRef = node;
 	}
+
 
 	handleClick(text) {
 		this.setState({ currentVal: text });
 		this.setState({ opened: false });
 	}
+
 
 	handleClickOutside(event) {
 		if (this.menuRef && (!this.menuRef.contains(event.target))) {
@@ -59,22 +70,36 @@ class CurrencySelect extends React.Component {
 					id="currency-select"
 					className="currency-select"
 					pullRight
-					onToggle={() => this.toggleDropdown()}
+					// onToggle={() => this.toggleDropdown()}
+					onToggle={() => true} // TAB close fix
 					open={this.state.opened}
+					onKeyPress={() => { console.log('key press'); }}
 				>
-					<Dropdown.Toggle onClick={() => this.toggleDropdown()}>
+					<Dropdown.Toggle
+						onClick={() => this.toggleDropdown()}
+					>
 						<span className="val">{(currentVal) || 'ECHO'}</span>
 					</Dropdown.Toggle>
 					<CustomMenu bsRole="menu">
 						<div className="menu-container">
-							<Input
+							{/* <Input
 								placeholder="Type asset or token name"
 								onClick={(e) => { e.stopPropagation(); e.preventDefault(); }}
-							/>
+								focus
+								tabIndex={0}
+							/> */}
+							<div className="ui input">
+								<input
+									type="text"
+									tabIndex={0}
+									placeholder="Type asset or token name"
+									onClick={(e) => { e.stopPropagation(); e.preventDefault(); }}
+									ref={(node) => { this.searchInput = node; }}
+								/>
+							</div>
 							<div className="select-container">
 								<div
 									className="user-scroll"
-									// style={{ height: '165px' }}
 									style={{ height: '118px' }}
 								>
 									<CustomScroll
@@ -83,18 +108,28 @@ class CurrencySelect extends React.Component {
 									>
 										{
 											data.map((elem) => (
-												<div key={elem.id} className="select-item">
+												<div
+													key={elem.id}
+													className="select-item"
+												>
 													<div className="title">{elem.title}</div>
 													<ul>
 														{
-															elem.list.map((text, i) => (
-																<MenuItem
-																	eventKey={i}
-																	key={Math.random()}
-																	onClick={(e) => { this.handleClick(text); e.preventDefault(); }}
-																>
-																	{text}
-																</MenuItem>
+															elem.list.map((text) => (
+																<li key={Math.random()}>
+																	<a
+																		href=""
+																		tabIndex={0}
+																		onClick={(e) => { this.handleClick(text); e.preventDefault(); }}
+																		onKeyPress={
+																			(e) => {
+																				this.onItemKeyPress(e, text); e.preventDefault();
+																			}
+																		}
+																	>
+																		{text}
+																	</a>
+																</li>
 															))
 														}
 													</ul>
