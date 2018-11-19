694c6fc2c9abf788fbedbcf33b00ea8a828cdf1e
Dmitry Karpushkin <d.karpushkin@pixelplex.by>
2018-11-19 16:12:56 +0300

    Fixes BRG-130

--- src/index.js
+++ src/index.js
@@ -10,6 +10,12 @@ import './assets/loader';
 import history from './history';
 import store from './store';
 
+import { POPUP_HEIGHT, POPUP_WIDTH } from './constants/GlobalConstants';
+
+if (window.innerWidth !== window.outerWidth || window.innerHeight !== window.outerHeight) {
+	window.resizeBy(POPUP_WIDTH - window.innerWidth, POPUP_HEIGHT - window.innerHeight);
+}
+
 ReactDOM.render(
 	<Provider store={store}>
 		{/* ConnectedRouter will use the store from Provider automatically */}
