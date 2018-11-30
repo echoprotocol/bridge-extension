16f0a62b9f86f17dc81f633be8eaf32179a5e43d
Dmitry Karpushkin <d.karpushkin@pixelplex.by>
2018-11-30 20:05:02 +0300

    fix red window

--- src/containers/Send/SuccessTransaction.jsx
+++ src/containers/Send/SuccessTransaction.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'semantic-ui-react';
+import { connect } from 'react-redux';
 
-import { closePopup } from '../../actions/SignActions';
+import { closePopup, globals } from '../../actions/SignActions';
+import { POPUP_WINDOW_TYPE } from '../../constants/GlobalConstants';
 
 class SuccessTransaction extends React.PureComponent {
 
 	onClick() {
 		closePopup();
+
+		if (globals.WINDOW_TYPE === POPUP_WINDOW_TYPE && !this.props.transaction) {
+			return null;
+		}
+
 		this.props.history.goBack();
+
+		return null;
 	}
 
 	render() {
+		console.log(this.props.transaction);
 		return (
 			<div className="transaction-status-wrap success">
 				<div className="transaction-status-body">
@@ -37,6 +47,13 @@ class SuccessTransaction extends React.PureComponent {
 
 SuccessTransaction.propTypes = {
 	history: PropTypes.object.isRequired,
+	transaction: PropTypes.any,
 };
 
-export default SuccessTransaction;
+SuccessTransaction.defaultProps = {
+	transaction: null,
+};
+
+export default connect((state) => ({
+	transaction: state.global.getIn(['sign', 'current']),
+}))(SuccessTransaction);
