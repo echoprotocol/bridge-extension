b25ce5637ac97604faffa16cb7a716b61d9a24b5
Andrey Fedorovich <afedorovich@Andreys-iMac.local>
2019-01-15 17:44:53 +0300

    BRG-166

--- src/containers/Send/index.jsx
+++ src/containers/Send/index.jsx
@@ -281,7 +281,7 @@ class Send extends React.Component {
 									name="memo"
 									value={memo.value}
 									onChange={(e) => this.onChange(e)}
-									label="Note (optional)"
+									label="Note (Optional)"
 									error={!!memo.error}
 									errorText={memo.error}
 									disabled={loading}
