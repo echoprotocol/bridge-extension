ecc1928aa3317e19a766b7c23fea3dcb0476174d
Andrey Fedorovich <a.fedorovich@pixelplex.io>
2019-02-11 15:57:22 +0300

    Resolve BRG-241

--- src/containers/Transactions/index.jsx
+++ src/containers/Transactions/index.jsx
@@ -1,7 +1,4 @@
-/* eslint-disable no-nested-ternary */
-
 import React from 'react';
-import { Accordion } from 'semantic-ui-react';
 import CustomScroll from 'react-custom-scroll';
 import { withRouter } from 'react-router';
 import { connect } from 'react-redux';
@@ -25,12 +22,10 @@ class Transactions extends React.Component {
 			note: '',
 			noteId: '',
 		};
-		this.handleClick = this.handleClick.bind(this);
 	}
 
-	async handleClick(e, titleProps) {
+	async getTransactionInfo(e, index) {
 
-		const { index } = titleProps;
 		const { activeId, noteId } = this.state;
 		const newId = activeId === index ? -1 : index;
 
@@ -72,7 +67,7 @@ class Transactions extends React.Component {
 			return 0;
 		});
 
-		const { activeId, note } = this.state;
+		const { note, activeId } = this.state;
 		const { accountName } = this.props;
 
 		return (
@@ -82,36 +77,41 @@ class Transactions extends React.Component {
 						flex="1"
 						heightRelativeToParent="calc(100%)"
 					>
-						<div className="transactions-wrapper">
-							<Accordion>
+						<div className="transactions-wrap">
+							<div className="accordion">
 								{
 									ordered.toArray().map((elem) =>
 										(
-											<React.Fragment key={elem.get('id')}>
-												<Accordion.Title
-													active={activeId === elem.get('id')}
-													index={elem.get('id')}
-													onClick={this.handleClick}
+											<div className="accordion-item" key={elem.get('id')}>
+												<input
+													onChange={(e) => { this.getTransactionInfo(e, elem.get('id')); }}
+													id={elem.get('id')}
+													type="checkbox"
+													name="transactions"
+													checked={elem.get('id') === activeId}
+												/>
+												<label
+													className="transaction-element"
+													htmlFor={elem.get('id')}
 												>
-													<div className="transaction-element">
-														<div className="top-block">
-															<div className="transaction-type">
-																<img src={`/images/pic${elem.getIn(['transaction', 'type'])}.svg`} alt="" />
-																<div className={`icon-Pic${elem.getIn(['transaction', 'type'])}`} />
-																{elem.getIn(['transaction', 'typeName'])}
-															</div>
-															<div className="transaction-date">{elem.getIn(['transaction', 'date'])}</div>
+													<div className="top-block">
+														<div className="transaction-type">
+															<img src={`/images/pic${elem.getIn(['transaction', 'type'])}.svg`} alt="" />
+															<div className={`icon-Pic${elem.getIn(['transaction', 'type'])}`} />
+															{elem.getIn(['transaction', 'typeName'])}
 														</div>
-														<div className="bottom-block">
-															<div className="transaction-value">
-																{elem.getIn(['transaction', 'value'])}
-																<span className="currency">{elem.getIn(['transaction', 'currency'])}</span>
-															</div>
-															<img className="ddDown" src={downArrow} alt="" />
-														</div>
+														<div className="transaction-date">{elem.getIn(['transaction', 'date'])}</div>
 													</div>
-												</Accordion.Title>
-												<Accordion.Content active={activeId === elem.get('id')}>
+													<div className="bottom-block">
+														<div className="transaction-value">
+															{elem.getIn(['transaction', 'value'])}
+															<span className="currency">{elem.getIn(['transaction', 'currency'])}</span>
+														</div>
+														<img className="ddDown" src={downArrow} alt="" />
+													</div>
+												</label>
+
+												<div className="content">
 													<div className="transaction-element-content">
 														{
 															accountName !== elem.getIn(['content', 'sender']) ?
@@ -142,10 +142,10 @@ class Transactions extends React.Component {
 															}
 														</div>
 													</div>
-												</Accordion.Content>
-											</React.Fragment>))
+												</div>
+											</div>))
 								}
-							</Accordion>
+							</div>
 						</div>
 					</CustomScroll>
 				</div>
