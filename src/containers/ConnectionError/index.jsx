ead4f850872d5502e85f4ad8e9d9d29baf53731e
Andrey Fedorovich <a.fedorovich@pixelplex.io>
2019-03-12 16:36:22 +0300

    BRG-329 fix tryToConnect

--- src/containers/ConnectionError/index.jsx
+++ src/containers/ConnectionError/index.jsx
@@ -7,6 +7,24 @@ import { changeNetwork } from '../../actions/GlobalActions';
 
 class ConnectionError extends React.Component {
 
+	constructor(props) {
+		super(props);
+		this.state = {
+			loading: false,
+		};
+	}
+
+	async tryToConnect() {
+		this.setState({ loading: true });
+		try {
+			await this.props.tryToConnect();
+
+		} catch (error) {
+			console.log('tryToConnect error: ', error);
+		} finally {
+			this.setState({ loading: false });
+		}
+	}
 
 	render() {
 
@@ -16,7 +34,8 @@ class ConnectionError extends React.Component {
 				<div className="one-btn-wrap" >
 					<Button
 						content={<span className="btn-text">Try Again</span>}
-						onClick={() => this.props.tryToConnect()}
+						onClick={() => this.tryToConnect()}
+						disabled={this.state.loading}
 						className="btn-in-light"
 					/>
 				</div>
