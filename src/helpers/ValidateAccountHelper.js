f1323da369915d88a844c4a5440753b433e83b0e
kosAndre22 <a.kosobuckiy@pixelplex.io>
2019-11-20 13:17:14 +0300

    prohibition to register expensive accs

--- src/helpers/ValidateAccountHelper.js
+++ src/helpers/ValidateAccountHelper.js
@@ -3,7 +3,9 @@ import { validators } from 'echojs-lib';
 class ValidateAccountHelper {
 
 	static validateAccountName(accountName) {
-
+		if (!(/[.\-/0-9]/.test(accountName) || !accountName.match(/[aeiouy]/ig))) {
+			return 'Account name should contain digit, number, dash, slash or consist only of consonants';
+		}
 		if (!accountName) {
 			return 'Account name should not be empty';
 		}
