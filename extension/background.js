7ff361104d86f6cf6a45cc3e784f710509dd57a4
Mikhail Shautsou <m.shautsou@pixelplex.io>
2020-04-02 18:33:17 +0300

    [BRG-548] migrate to echo 0.18

--- extension/background.js
+++ extension/background.js
@@ -332,7 +332,7 @@ const createAccount = async (name, path) => {
 		}
 		const wif = crypto.generateWIF();
 		const key = PrivateKey.fromWif(wif).toPublicKey().toString();
-		await echoService.getChainLib().api.registerAccount(name, key, key);
+		await echoService.getChainLib().api.registerAccount(name, key, key, null);
 		await storage.set('account', { name, keys: [key], networkName: network.name });
 		await crypto.importByWIF(network.name, wif);
 		await emitter.emit('addAccount', name, [key], network.name, path);
